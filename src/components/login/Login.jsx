3bdd452ab2daf81978f4fc599083fcd0aeb69334
vsbron <vsbron@gmail.com>
2024-08-24 15:15:52 +0300

    Added the register form handler that creates user in the db and updates necessary tables

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+
+import { auth, db } from "../../lib/firebase";
 
 import Avatar from "../../ui/avatar/Avatar";
 
@@ -23,9 +27,45 @@ function Login() {
 
   // Login handler for the form
   const handleLogin = (e) => {
-    // Preventing default behaviour
+    // Preventing default behavior
     e.preventDefault();
+
+    // Showing the message
     toast.success("Test");
+  };
+
+  // Registration handler that creates the user in the database
+  const handleRegister = async (e) => {
+    // Preventing default behavior
+    e.preventDefault();
+
+    // Getting the form values from form to constants
+    const formData = new FormData(e.target);
+    const { username, email, password } = Object.fromEntries(formData);
+
+    try {
+      // Sending the request to create a new user
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      // Creating the entry in the user database with username, email, id and blocked users
+      await setDoc(doc(db, "users", res.user.uid), {
+        username,
+        email,
+        id: res.user.uid,
+        blocked: [],
+      });
+
+      // Creating the entry in the user chats database with the list of the chats
+      await setDoc(doc(db, "userchats", res.user.uid), {
+        chats: [],
+      });
+
+      // Showing success message
+      toast.success("Account created! You can log in now");
+    } catch (err) {
+      console.error(err);
+      toast.error(err.message);
+    }
   };
 
   // Returned JSX
@@ -47,7 +87,7 @@ function Login() {
       {/* Signing up part */}
       <div className="login__item">
         <h2>Create an Account</h2>
-        <form className="login__form">
+        <form className="login__form" onSubmit={handleRegister}>
           <label htmlFor="file" className="login__avatar">
             <Avatar size="5rem" avatarUrl={avatar.url} />
             Upload an image
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,8 +3,9 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// Configuring Firebase
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "react2024-modern-chat-app.firebaseapp.com",
   projectId: "react2024-modern-chat-app",
   storageBucket: "react2024-modern-chat-app.appspot.com",
@@ -12,6 +13,7 @@ const firebaseConfig = {
   appId: "1:515013875620:web:283b866cbfc916f9e9d331",
 };
 
+// Initializing the app
 const app = initializeApp(firebaseConfig);
 
 // Exporting the firebase tools
