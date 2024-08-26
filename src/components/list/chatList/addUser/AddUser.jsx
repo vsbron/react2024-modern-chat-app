4065f096300a8d8fd0ebc9461ece9c763c3a2d53
vsbron <vsbron@gmail.com>
2024-08-26 13:32:39 +0300

    Implemented the user search mechanics

--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+
+import { db } from "../../../../lib/firebase";
 import useCloseModal from "../../../../utils/useCloseModal";
 
 import Avatar from "../../../../ui/avatar/Avatar";
@@ -6,6 +10,9 @@ import Button from "../../../../ui/button/Button";
 import "./addUser.css";
 
 function AddUser({ setAddMode }) {
+  // Setting the state for the user
+  const [user, setUser] = useState();
+
   // Custom hook that adds the click handlers that will close the Add User module
   useCloseModal({
     setter: setAddMode,
@@ -13,23 +20,48 @@ function AddUser({ setAddMode }) {
     modalClass: "add-user",
   });
 
+  // Search submit handle
+  const handleSearch = async (e) => {
+    // Prevent default behavior
+    e.preventDefault();
+
+    // Getting the form data and the username
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+
+    try {
+      const userRef = collection(db, "users"); // Getting the users from database
+      const q = query(userRef, where("username", "==", username)); // Query for the username search
+      const querySnapshot = await getDocs(q); // Searching for the username that we entered
+
+      // If found user, set the state with user data
+      if (!querySnapshot.empty) {
+        setUser(querySnapshot.docs[0].data());
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   // Returned JSX
   return (
     <div className="add-user">
       {/* Input field for searching users */}
-      <form action="" className="add-user__form">
+      <form className="add-user__form" onSubmit={handleSearch}>
         <input type="text" placeholder="Username" name="username" />
         <Button padding="2rem">Search</Button>
       </form>
 
       {/* Found users list */}
-      <div className="add-user__user">
-        <div className="add-user__user-details">
-          <Avatar size="5rem" />
-          <span className="add-user__user-name">Jane Doe</span>
+      {user && (
+        <div className="add-user__user">
+          <div className="add-user__user-details">
+            <Avatar src={user.avatar} size="5rem" />
+            <span className="add-user__user-name">{user.username}</span>
+          </div>
+          <Button padding="1rem">Add user</Button>
         </div>
-        <Button padding="1rem">Add user</Button>
-      </div>
+      )}
     </div>
   );
 }
