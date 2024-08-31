8b02d03cb9230fc0b4d8ed48549f57a23de49878
vsbron <vsbron@gmail.com>
2024-08-31 21:02:17 +0300

    Adjusted colors. Fixed BG error. Fixed z-index errors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const COLORS = [
-  { name: "violet", bg: "#6d28d9" },
-  { name: "blue", bg: "#1d4ed8" },
-  { name: "orange", bg: "#c2410c" },
-  { name: "green", bg: "#15803d" },
+  { name: "violet", bg: "var(--color-button-violet)" },
+  { name: "blue", bg: "var(--color-button-blue)" },
+  { name: "orange", bg: "var(--color-button-orange)" },
+  { name: "green", bg: "var(--color-button-green)" },
 ];
